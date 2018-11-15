9057b75e5e083aa99fbc40fdb2d534e9033348d7
Whooolia <tpdms2779@gmail.com>
2018-11-15 13:02:30 +0000

    recipe-image fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-/* let's go! */
 
 // var xhr = new XMLHttpRequest();
 // var url =
@@ -14,20 +13,37 @@
 // // xhr.open("GET", url2, true);
 // xhr.send();
 
+var randomRecipe = Math.floor(Math.random()*10);
+var randomPageRecipe = Math.floor(Math.random()*100);
+
 var xhr2 = new XMLHttpRequest();
-var url2 =
-  "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/";
+// Use cors-anywhere to avoid CORS
+var url2 = 'https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?&p='+randomPageRecipe;
 
 xhr2.onreadystatechange = function() {
   if (xhr2.readyState == 4 && xhr2.status == 200) {
-    // document.getElementById("demo").innerHTML = xhr.responseText;
-    var obj = JSON.parse(xhr2.responseText);
-    var title = obj.title;
-    console.log("it works");
+    var receipeApi = JSON.parse(xhr2.responseText);
+    // Give a random number to get a rondom receipe title
+    var title = receipeApi.results[randomRecipe].title;
+    var ingredients = receipeApi.results[randomRecipe].ingredients;
+    var pic = receipeApi.results[randomRecipe].thumbnail;
+  
+console.log(receipeApi);
     document.getElementById("button").addEventListener("click", () => {
-      //   alert("great");
+        // the title displays on website
       document.getElementById("recipe_name").textContent = title;
-    });
+      document.getElementById("recipe_ingredients").textContent = ingredients;
+      if(pic !== ''){
+        var createImg = document.createElement('img');
+        document.getElementById('recipe_pic').appendChild(createImg);
+        createImg.setAttribute('src', pic)
+        // createImg.className = 'img'
+       
+        
+      }
+      
+
+});
   }
 };
 xhr2.open("GET", url2, true);
