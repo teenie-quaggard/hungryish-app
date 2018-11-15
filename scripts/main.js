0e22feebd63ea1428a5b21d7762a0813a3c802f1
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-11-15 12:12:01 +0000

    Refactor request into event listener and randomize selections

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,21 @@
-/* let's go! */
+/*****************************Movie shit******************************************/
+document.getElementById("button").addEventListener("click", () => {
+  const randomMovieGen = max => Math.floor(Math.random() * Math.floor(max));
+  let randomNum = randomMovieGen(20);
+  console.log(randomNum);
+  const movieRequest = new XMLHttpRequest();
+  const movieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=cb89d5264333858f9881aa9e8e0b5d15&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randomNum}&certification_country=US&certification=R&sort_by=vote_average.desc&sort_by=vote_count.desc`;
 
-var movieRequest = new XMLHttpRequest();
-var movieUrl =
-  "https://api.themoviedb.org/3/discover/movie?api_key=cb89d5264333858f9881aa9e8e0b5d15&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&certification_country=US&certification=R&sort_by=vote_average.desc&sort_by=vote_count.desc";
-movieRequest.onreadystatechange = function() {
-  if (movieRequest.readyState == 4 && movieRequest.status == 200) {
-    var movies = JSON.parse(movieRequest.responseText);
-    document.getElementById("button").addEventListener("click", () => {
+  movieRequest.onreadystatechange = function() {
+    if (movieRequest.readyState == 4 && movieRequest.status == 200) {
+      var movies = JSON.parse(movieRequest.responseText);
+      console.log(movies);
       document.getElementById("movie_name").textContent =
-        movies.results[0].title;
-    });
-  }
-};
-movieRequest.open("GET", movieUrl, true);
-movieRequest.send();
+        movies.results[randomMovieGen(20)].title;
+    }
+  };
+  movieRequest.open("GET", movieUrl, true);
+  movieRequest.send();
+});
 
-// var movieRequest2 = new XMLHttpRequest();
-// var url2 =
-//   "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/";
-//
-// xhr2.onreadystatechange = function() {
-//   if (xhr2.readyState == 4 && xhr2.status == 200) {
-//     // document.getElementById("demo").innerHTML = xhr.responseText;
-//     var obj = JSON.parse(xhr2.responseText);
-//     var title = obj.title;
-//     console.log("it works");
-//     document.getElementById("button").addEventListener("click", () => {
-//       //   alert("great");
-//       document.getElementById("recipe_name").textContent = title;
-//     });
-//   }
-// };
-// xhr2.open("GET", url2, true);
-// xhr2.send();
+/*****************************Movie shit end******************************************/
