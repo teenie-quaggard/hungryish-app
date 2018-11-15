7693bf5bdfd5904f232d627de5792c9970e677b2
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-11-15 11:19:14 +0000

    Add event listener to movie api request and return first movie

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,35 @@
 /* let's go! */
 
-// var xhr = new XMLHttpRequest();
-// var url =
-//   "https://api.themoviedb.org/3/discover/movie?api_key=cb89d5264333858f9881aa9e8e0b5d15&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&certification_country=US&certification=R&sort_by=vote_average.desc";
-// console.log(url);
-// xhr.onreadystatechange = function() {
-//   if (xhr.readyState == 4 && xhr.status == 200) {
-//     var aboutme = JSON.parse(xhr.responseText);
-//     document.getElementById("p").textContent = aboutme.page;
-//   }
-// };
-// xhr.open("GET", url, true);
-// // xhr.open("GET", url2, true);
-// xhr.send();
-
-var xhr2 = new XMLHttpRequest();
-var url2 =
-  "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/";
-
-xhr2.onreadystatechange = function() {
-  if (xhr2.readyState == 4 && xhr2.status == 200) {
-    // document.getElementById("demo").innerHTML = xhr.responseText;
-    var obj = JSON.parse(xhr2.responseText);
-    var title = obj.title;
-    console.log("it works");
+var movieRequest = new XMLHttpRequest();
+var movieUrl =
+  "https://api.themoviedb.org/3/discover/movie?api_key=cb89d5264333858f9881aa9e8e0b5d15&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&certification_country=US&certification=R&sort_by=vote_average.desc&sort_by=vote_count.desc";
+movieRequest.onreadystatechange = function() {
+  if (movieRequest.readyState == 4 && movieRequest.status == 200) {
+    var movies = JSON.parse(movieRequest.responseText);
     document.getElementById("button").addEventListener("click", () => {
-      //   alert("great");
-      document.getElementById("recipe_name").textContent = title;
+      document.getElementById("movie_name").textContent =
+        movies.results[0].title;
     });
   }
 };
-xhr2.open("GET", url2, true);
-xhr2.send();
+movieRequest.open("GET", movieUrl, true);
+movieRequest.send();
+
+// var movieRequest2 = new XMLHttpRequest();
+// var url2 =
+//   "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/";
+//
+// xhr2.onreadystatechange = function() {
+//   if (xhr2.readyState == 4 && xhr2.status == 200) {
+//     // document.getElementById("demo").innerHTML = xhr.responseText;
+//     var obj = JSON.parse(xhr2.responseText);
+//     var title = obj.title;
+//     console.log("it works");
+//     document.getElementById("button").addEventListener("click", () => {
+//       //   alert("great");
+//       document.getElementById("recipe_name").textContent = title;
+//     });
+//   }
+// };
+// xhr2.open("GET", url2, true);
+// xhr2.send();
