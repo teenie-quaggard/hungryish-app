6fd36e9538de10568ee7fcbdf3a7a31565251932
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-11-15 12:55:57 +0000

    addd image, description and rating closes #4

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 /*****************************Movie shit******************************************/
 document.getElementById("button").addEventListener("click", () => {
-
   //generates a random number for both page and title within object
   const randomNumGen = max => Math.floor(Math.random() * Math.floor(max));
 
@@ -13,20 +12,31 @@ document.getElementById("button").addEventListener("click", () => {
     config.movieAPI
   }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randomPage}&certification_country=US&certification=R&sort_by=vote_average.desc&sort_by=vote_count.desc`;
 
-
   movieRequest.onreadystatechange = function() {
+    let randomMovie = randomNumGen(20);
     if (movieRequest.readyState == 4 && movieRequest.status == 200) {
-      var movies = JSON.parse(movieRequest.responseText);
+      const movies = JSON.parse(movieRequest.responseText);
+      console.log(movies);
 
       //Adds title of movie to page
       document.getElementById("movie_name").textContent =
-        movies.results randomNumGen(20)].title;
-
-      //Adds poster of movie to page
+        movies.results[randomMovie].title;
 
       //Adds average rating to page
+      document.getElementById("movie_rating").textContent = `Rating: ${
+        movies.results[randomMovie].vote_average
+      }`;
+
+      //Adds poster of movie to pages
+      let movieImage = movies.results[randomMovie].poster_path;
+      document.getElementById(
+        "movie_poster"
+      ).src = `http://image.tmdb.org/t/p/w200/${movieImage}`;
 
       //Adds description of movie to page
+      document.getElementById("movie_description").textContent = `${
+        movies.results[randomMovie].overview
+      }`;
     }
   };
   movieRequest.open("GET", movieUrl, true);
