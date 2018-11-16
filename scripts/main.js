1283357bd4c17ce5715fcc72fb3f285177e2b018
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-11-16 11:55:21 +0000

    Change var to const and add strict to top of js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //generates a random number for use within functions
 const randomNumGen = max => Math.floor(Math.random() * Math.floor(max));
 
@@ -6,22 +8,22 @@ const randomNumGen = max => Math.floor(Math.random() * Math.floor(max));
 document.getElementById("recipe_image").style.display = "none";
 
 document.getElementById("button").addEventListener("click", () => {
-  var randomRecipe = randomNumGen(10);
-  var randomPageRecipe = randomNumGen(100);
+  const randomRecipe = randomNumGen(10);
+  const randomPageRecipe = randomNumGen(100);
 
-  var xhr2 = new XMLHttpRequest();
+  const xhr2 = new XMLHttpRequest();
   // Use cors-anywhere to avoid CORS
-  var url2 =
+  const url2 =
     "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?&p=" +
     randomPageRecipe;
 
   xhr2.onreadystatechange = function() {
     if (xhr2.readyState == 4 && xhr2.status == 200) {
-      var recipeApi = JSON.parse(xhr2.responseText);
+      const recipeApi = JSON.parse(xhr2.responseText);
       // Give a random number to get a random receipe title
-      var title = recipeApi.results[randomRecipe].title;
-      var ingredients = recipeApi.results[randomRecipe].ingredients;
-      var link = recipeApi.results[randomRecipe].href;
+      const title = recipeApi.results[randomRecipe].title;
+      const ingredients = recipeApi.results[randomRecipe].ingredients;
+      const link = recipeApi.results[randomRecipe].href;
       const gifSearch = title.substr(0, title.indexOf(" "));
 
       document.getElementById("recipe_ingredients").textContent = ingredients;
@@ -39,8 +41,8 @@ document.getElementById("button").addEventListener("click", () => {
 
       giphyApi.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          let giphy = document.getElementById("recipe_image");
-          gifObj = JSON.parse(giphyApi.responseText);
+          const giphy = document.getElementById("recipe_image");
+          const gifObj = JSON.parse(giphyApi.responseText);
           giphy.src = gifObj.data[randomGIF].images.fixed_width.url;
         }
       };
@@ -63,7 +65,7 @@ document.getElementById("movie_poster").style.display = "none";
 
 document.getElementById("button").addEventListener("click", () => {
   //generates a random page under 20 - can increase to access more movies
-  let randomPage = randomNumGen(20);
+  const randomPage = randomNumGen(20);
 
   //api request using a randomPage
   const movieRequest = new XMLHttpRequest();
@@ -72,7 +74,7 @@ document.getElementById("button").addEventListener("click", () => {
   }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${randomPage}&certification_country=US&certification=R&sort_by=vote_average.desc&sort_by=vote_count.desc`;
 
   movieRequest.onreadystatechange = function() {
-    let randomMovie = randomNumGen(20);
+    const randomMovie = randomNumGen(20);
     if (movieRequest.readyState == 4 && movieRequest.status == 200) {
       const movies = JSON.parse(movieRequest.responseText);
 
@@ -86,7 +88,7 @@ document.getElementById("button").addEventListener("click", () => {
       }`;
 
       //Adds poster of movie to pages
-      let movieImage = movies.results[randomMovie].poster_path;
+      const movieImage = movies.results[randomMovie].poster_path;
       document.getElementById(
         "movie_poster"
       ).src = `http://image.tmdb.org/t/p/w200/${movieImage}`;
